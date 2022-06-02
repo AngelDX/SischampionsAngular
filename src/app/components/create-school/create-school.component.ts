372f6c2d115f1eebd17226db7a228a3e98630bb3
AngelDX <angel.condori@gmail.com>
2022-06-02 12:20:00 -0500

    crud terminado

--- src/app/components/create-school/create-school.component.ts
+++ src/app/components/create-school/create-school.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { School } from 'src/app/models/school.model';
+import { SchoolService } from 'src/app/services/school.service';
 
 @Component({
   selector: 'app-create-school',
@@ -7,9 +11,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateSchoolComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public schoolService:SchoolService,
+    private router:Router) { }
 
   ngOnInit(): void {
+
+  }
+
+  submitForm(schoolForm:NgForm){
+    if(schoolForm.value.id==null){
+      this.schoolService.CreateSchool(schoolForm.value)
+      .subscribe((response)=>{
+        this.router.navigate([""]);
+      });
+    }else{
+      this.schoolService.UpdateSchool(schoolForm.value.id,schoolForm.value)
+      .subscribe((response)=>{
+        this.router.navigate([""]);
+      });
+    }
+    this.resetForm(schoolForm);
+  }
+
+  resetForm(schoolForm:NgForm){
+    if(schoolForm!=null){
+      schoolForm.reset();
+      this.schoolService.selectSchool=new School();
+    }
   }
 
 }
--- src/app/components/list-school/list-school.component.ts
+++ src/app/components/list-school/list-school.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { School } from 'src/app/models/school.model';
+import { SchoolService } from 'src/app/services/school.service';
 
 @Component({
   selector: 'app-list-school',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-school.component.css']
 })
 export class ListSchoolComponent implements OnInit {
-
-  constructor() { }
+  schoolList:any=[];
+  constructor(public schoolService:SchoolService,private router:Router) { }
 
   ngOnInit(): void {
+    this.loadSchools();
   }
 
+  loadSchools(){
+    return this.schoolService.GetSchools().subscribe((data:{})=>{
+      this.schoolList=data;
+      console.log(data);
+    });
+  }
+
+  deleteSchool(id:number){
+    this.schoolService.DeleteSchool(id).subscribe((response)=>{
+      this.loadSchools();
+    });
+  }
+
+  onEdit(school:School){
+    console.log(school);
+    this.schoolService.selectSchool=Object.assign({},school);
+    this.router.navigate(["/add-school"]);
+  }
 }
